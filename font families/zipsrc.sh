1247b007b4cc88f06fd25a2ecc2ffd98bb9d46f5
Nicole Rossi <i.am.nicole.rossi@gmail.com>
2024-12-13 12:17:53 -0800

    Commit before proofreading/editing
    Also added zip script and moved unused example text to "font size example"

---
+++ font families/zipsrc.sh
@@ -0,0 +1,9 @@
+#!/usr/local/bin/bash
+rm "font families example.zip"
+mkdir "font families example"
+cp commands.tex "font families example/commands.tex"
+cp commands.pdf "font families example/commands.pdf"
+cp switches.tex "font families example/switches.tex"
+cp switches.pdf "font families example/switches.pdf"
+zip -r "font families example.zip" "font families example"
+rm -rf "font families example"
