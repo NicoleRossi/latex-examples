0eca94ad1990e67904d1b5093eea2d90ba2ccb3a
Nicole Rossi <i.am.nicole.rossi@gmail.com>
2024-12-16 10:04:16 -0800

    Add bold switches example
    Removed old files in font size example (commands vs switches is obsolete).

---
+++ bold switches/zipsrc.sh
@@ -0,0 +1,6 @@
+rm "bold switches example.zip"
+mkdir "bold switches example"
+cp "bold switches.tex" "bold switches example/bold switches.tex"
+cp "bold switches.pdf" "bold switches example/bold switches.pdf"
+zip -r "bold switches example.zip" "bold switches example"
+rm -rf "bold switches example"
