ebe6cc0ad1c57c70ba6c980f28ac91d26ba5e2e0
Nicole Rossi <i.am.nicole.rossi@gmail.com>
2024-12-16 08:57:48 -0800

    Revert "Simplify font size example"

--- font sizes/zipsrc.sh
+++
@@ -1,6 +0,0 @@
-rm "font sizes example.zip"
-mkdir "font sizes example"
-cp "font sizes.tex" "font sizes example/font sizes.tex"
-cp "font sizes.pdf" "font sizes example/font sizes.pdf"
-zip -r "font sizes example.zip" "font sizes example"
-rm -rf "font sizes example"
